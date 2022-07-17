6955de22e3fa8f3285974363398df8ff6bd6b7ee
vulcan-dev <vitexuhd@gmail.com>
2022-07-17 05:50:52 +0100

    Improved `Markdown` Generator

--- src/md.rs
+++ src/md.rs
@@ -11,19 +11,22 @@ pub fn gen(api: &API) {
 
         if let Some(input) = &function.input {
             contents.push_str(&format!("### Input\n\n"));
-            contents.push_str(&format!("| Name | Type |\n"));
-            contents.push_str(&format!("| ---- | ---- |\n"));
+            contents.push_str(&format!("| Name | Type | Optional | Description |\n"));
+            contents.push_str(&format!("| ---- | ---- | -------- | ----------- |\n"));
             for param in input {
-                contents.push_str(&format!("| {} | {} |\n", param.name, param.type_));
+                contents.push_str(&format!("| {} | {} | {} | {} |\n", param.name, param.type_, param.optional, param.desc));
             }
+        } else {
+            contents.push_str(&format!("### Input\n\n"));
+            contents.push_str(&format!("No input\n\n"));
         }
 
         if let Some(output) = &function.output {
             contents.push_str(&format!("### Output\n\n"));
-            contents.push_str(&format!("| Name | Type |\n"));
-            contents.push_str(&format!("| ---- | ---- |\n"));
+            contents.push_str(&format!("| Name | Type | Description |\n"));
+            contents.push_str(&format!("| ---- | ---- | ----------- |\n"));
             for param in output {
-                contents.push_str(&format!("| {} | {} |\n", param.name, param.type_));
+                contents.push_str(&format!("| {} | {} | {} |\n", param.name, param.type_, param.desc));
             }
         }
     }
